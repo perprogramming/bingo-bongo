640c2632198417df661320e1482436dfd2299237
Per Bernhardt <pb@pixelhouse.de>
2015-07-19 12:25:00 +0200

    Deployment Test 11

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,6 @@
 openssl aes-256-cbc -K $encrypted_25303b6f6937_key -iv $encrypted_25303b6f6937_iv -in travis/id_rsa.enc -out travis/id_rsa -d
 chmod go-rwx travis/id_rsa
 
+rm -rf .git
+
 scp -v -r -i travis/id_rsa . ssh-49905-plb@sinfin.de:webseiten/bingo-bongo.perprogramming.de
