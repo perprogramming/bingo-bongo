c7501e967505d197afbf65c715e11f9f3843540f
Per Bernhardt <pb@pixelhouse.de>
2015-07-19 12:17:32 +0200

    Deployment Test 7

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-ls -alh ~/.ssh
+openssl aes-256-cbc -K $encrypted_25303b6f6937_key -iv $encrypted_25303b6f6937_iv -in travis/id_rsa.enc -out travis/id_rsa -d
 
-
-scp -r . ssh-49905-plb@sinfin.de:/webseiten/bingo-bongo.perprogramming.de
+scp -r -i travis/id_rsa . ssh-49905-plb@sinfin.de:/webseiten/bingo-bongo.perprogramming.de
