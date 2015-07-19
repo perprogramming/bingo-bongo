9a9a265c48dc03f49230724f265f26c58ada3f9f
Per Bernhardt <pb@pixelhouse.de>
2015-07-19 12:28:57 +0200

    Deployment fertig

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,4 @@
 openssl aes-256-cbc -K $encrypted_25303b6f6937_key -iv $encrypted_25303b6f6937_iv -in travis/id_rsa.enc -out travis/id_rsa -d
 chmod go-rwx travis/id_rsa
 
-scp -v -r -i travis/id_rsa . ssh-49905-plb@sinfin.de:webseiten/bingo-bongo.perprogramming.de
+scp -v -r -i travis/id_rsa $(ls -1) ssh-49905-plb@sinfin.de:webseiten/bingo-bongo.perprogramming.de
